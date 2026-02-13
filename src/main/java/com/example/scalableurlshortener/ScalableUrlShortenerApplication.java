cf55d97719899793f24df74a488b0a8e61d3a265
Uday <pudaykiran209@gmail.com>
2026-02-13 13:00:33 -0500

    Health Controller

---
+++ src/main/java/com/example/scalableurlshortener/ScalableUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scalableurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableUrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableUrlShortenerApplication.class, args);
+	}
+
+}
